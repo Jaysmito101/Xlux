798c9b504ac3880d18b6a8857d441af07c9a5fe0
Jaysmito101 <jaysmito101@gmail.com>
2026-05-14 00:40:29 +0530

    fix: update format.bat to include 'tiny_obj_loader' in formatting exclusions

--- format.bat
+++ format.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Formatting C++ files...
-for /f "delims=" %%f in ('dir /s /b *.c *.cpp *.h *.hpp 2^>nul ^| findstr /v /i "\\Vendor\\ stb_image cgltf \\build\\ \\build.ninja\\ "') do (
+for /f "delims=" %%f in ('dir /s /b *.c *.cpp *.h *.hpp 2^>nul ^| findstr /v /i "\\Vendor\\ stb_image cgltf tiny_obj_loader \\build\\ \\build.ninja\\ "') do (
     echo Formatting: %%f
     clang-format -i "%%f"
 )
