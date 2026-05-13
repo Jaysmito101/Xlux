560da50a331f824f1f0888ce6ea920cdb6599dbb
Jaysmito101 <jaysmito101@gmail.com>
2026-05-14 00:12:42 +0530

    fix: add default destructor to IFramebuffer interface

--- XluxEngine/Include/Impl/Framebuffer.hpp
+++ XluxEngine/Include/Impl/Framebuffer.hpp
@@ -20,5 +20,6 @@ namespace xlux
 		virtual void GetColorPixel(I32 channel, I32 x, I32 y, F32& r, F32& g, F32& b, F32& a) const = 0;
 		virtual void GetDepthPixel(I32 x, I32 y, F32& depth) const { (void)x, (void)y, (void)depth, throw std::runtime_error("Not implemented"); }
 
+		virtual ~IFramebuffer() = default;
 	};
 }
