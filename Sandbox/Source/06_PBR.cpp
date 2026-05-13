cf083f7e21e59a24789cdb7988fd617da4e2efa4
Jaysmito101 <jaysmito101@gmail.com>
2026-05-14 00:13:50 +0530

    fix: reorder constructor initialization for VertexInData for consistency

--- Sandbox/Source/06_PBR.cpp
+++ Sandbox/Source/06_PBR.cpp
@@ -17,11 +17,11 @@ struct VertexInData
 	xlux::math::Vec3 bitangent;
 
 	VertexInData(xlux::math::Vec3 pos, xlux::math::Vec3 normal, xlux::math::Vec3 texCoord)
-		: position(pos), texCoord(texCoord), normal(normal), tangent(0.0f, 0.0f, 0.0f), bitangent(0.0f, 0.0f, 0.0f)
+		: position(pos), normal(normal), texCoord(texCoord), tangent(0.0f, 0.0f, 0.0f), bitangent(0.0f, 0.0f, 0.0f)
 	{}
 
 	VertexInData()
-		: position(0.0f, 0.0f, 0.0f), texCoord(0.0f, 0.0f, 0.0f), normal(0.0f, 0.0f, 0.0f)
+		: position(0.0f, 0.0f, 0.0f), normal(0.0f, 0.0f, 0.0f), texCoord(0.0f, 0.0f, 0.0f)
 	{}
 };
 
